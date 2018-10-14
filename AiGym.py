b69f1bc313df51a46f375105a2709765b0a1227b
Bryan <br.deridder@gmail.com>
2018-10-15 00:10:04 +0200

    Added q learning

--- AiGym.py
+++ AiGym.py
@@ -1,6 +1,7 @@
 import gym
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Agent:
@@ -14,7 +15,7 @@ class Agent:
             oldState = self.env.reset()
             print("Episode: " + str(n))
             for t in range(100):
-                action = self.strategy.nextAction()  # strategy should decide next action here
+                action = self.strategy.next_action()  # strategy should decide next action here
                 newState, reward, done, info = self.env.step(action)
                 percept = Percept(oldState, reward, action, newState, done, info.get("prob"))
                 print(percept)
@@ -24,6 +25,19 @@ class Agent:
                 if done:
                     print(f"Episode {n} finished after {t+1} timesteps")
                     break
+        self.test()
+
+    def test(self):
+        # testing / printing:
+        v = self.strategy.v
+        x = np.array([v[0:4], v[4:8], v[8:12], v[12:16]])
+        fig, ax = plt.subplots()
+        ax.imshow(x, cmap='hot', interpolation='nearest')
+        for i in range(0, 4):
+            for j in range(0, 4):
+                ax.text(j, i, round(x[i, j], 4),
+                        ha="center", va="center", color="b")
+        plt.show()
 
 
 class MarkovDecisionProcess:
@@ -34,7 +48,7 @@ class MarkovDecisionProcess:
         self.stateTransitionModel = np.zeros((amountOfStates, amountOfActions, amountOfStates))
 
     def update(self, percept):
-        self.stateTransitionModel[percept.oldState][percept.action][percept.nextState] = round(percept.prob,2)
+        self.stateTransitionModel[percept.oldState][percept.action][percept.nextState] = round(percept.prob, 2)
 
     def __str__(self) -> str:
         return "Actions: \n" + str(self.stateTransitionModel) + "\nRewardPerState:\n" + str(self.rewardPerState)
@@ -64,25 +78,34 @@ class Strategy:
 
     def __init__(self):
         self.mdp = MarkovDecisionProcess(16, 4)
-        self.policy = np.array()
+        self.v = np.zeros(16)
+        self.q = np.zeros((16, 4))
+        self.gamma = 0.9  # discount factor -> determines the importance of future rewards.
+        self.alpha = 0.1  # learning rate, determines to what extent new info override old info.
 
-    def nextAction(self):
+    def next_action(self):
+        # get next action from policy here
         return random.choice(self.mdp.actions)
 
     def learn(self, percept):
+        self.evaluate(percept)
+        self.improve()
+
+    def evaluate(self, percept: Percept):
         self.mdp.update(percept)
+        s = percept.oldState
+        sn = percept.nextState
+        a = percept.action
+        r = percept.reward
+        self.q[s, a] += self.alpha * (r + self.gamma * (self.q[sn, 0:4].max() - self.q[s, a]))
 
-        # test here
-        print(self.mdp.stateTransitionModel[0][1][1])
-
-    def evaluate(self):
-        pass
+        for i in range(0, 16):
+            self.v[i] = self.q[i, 0:4].max()
 
     def improve(self):
+        # improve policy here
         pass
-        # def evaluatie learn roept deze aan
-        # def improve learn roept deze aan
 
 
-agent = Agent('FrozenLake-v0', episodes=100)
+agent = Agent('FrozenLake-v0', episodes=1000)
 agent.learn()
