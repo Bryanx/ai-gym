8fda71ea718ee086b5b6700642d807b74bbaeedc
jarne <jarne@LAPTOP-40O2DF4T>
2018-11-20 14:07:01 +0100

    Update strategies/valueIterationStrategy.py

--- strategies/valueIterationStrategy.py
+++ strategies/valueIterationStrategy.py
@@ -13,7 +13,7 @@ class Strategy:
         self.γ = 0.9  # discount factor -> determines the importance of future rewards.
         self.α = 0.1  # learning rate, determines to what extent new info overrides old info.
         self.π = np.full((16, 4), 0.25)
-        self.ξ = 0  # TODO: fijnheid bepalen
+        self.ξ = 0.1  # fineness factor
 
     def next_action(self):
         # get next action from policy here
@@ -27,19 +27,21 @@ class Strategy:
     def evaluate(self, percept: Percept):
         Δ = 5000000
         rmax = self.mdp.rewardPerState.max
-        while Δ > self.ξ * rmax * (1-self.γ/self.γ):
+        while Δ > self.ξ * rmax * (1 - self.γ / self.γ):
             print("delta: " + Δ)
             Δ = 0
             for state in range(0, 16):
-                oldValues = self.v
-                self.v = self.valueFunction()
-                Δ = max(oldValues - self.v)
+                old_values = self.v
+                self.v = self.valueFunction(percept)
+                Δ = max(old_values - self.v)
 
-    def valueFunction(self):
-        # TODO: implement bellman funcion
+    def valueFunction(self, percept: Percept):
         utilValues = np.zeros(16)
         for i in range(0, 16):
-            pass
+            utilValues[i] = self.mdp.stateTransitionModel[percept.oldState, percept.action, percept.nextState]\
+                            * percept.reward\
+
+        return utilValues
 
     def improve(self):
         pass
