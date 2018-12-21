1500e04b2110db503e8765ad36754a4c00ba2c18
Jarne <jarne.vanaerde@hotmail.com>
2018-12-21 15:30:59 +0100

    working on mdp tests

--- test/test_mdp.py
+++ test/test_mdp.py
@@ -5,14 +5,12 @@ from src.model.percept import Percept
 
 
 class TestMDP(unittest.TestCase):
+    percept: Percept = Percept(old_state=14, reward=1, action=2, next_state=15, finished=True, prob=0.33)
+    mdp: MarkovDecisionProcess = MarkovDecisionProcess(state_count=16, action_count=4)
+    mdp.update(percept)
+
     def test_reward_per_state(self):
-        self.percept: Percept = Percept(old_state=15, reward=1, action=2, next_state=16, finished=True, prob=0.33)
-        self.mdp: MarkovDecisionProcess = MarkovDecisionProcess(state_count=16, action_count=4)
-        self.mdp.update(self.percept)
         self.assertEqual(1, 1)
 
     def test_state_transitioning_model(self):
-        elf.percept: Percept = Percept(old_state=15, reward=1, action=2, next_state=16, finished=True, prob=0.33)
-        self.mdp: MarkovDecisionProcess = MarkovDecisionProcess(state_count=16, action_count=4)
-        self.mdp.update(self.percept)
         self.assertEqual(1, 1)
